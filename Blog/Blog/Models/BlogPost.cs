16a07e8b5b4552b76997b6e66352c2c4251ddf60
Jafet-Picado <jafethpqpq26@gmail.com>
2023-06-01 13:05:06 -0600

    add nullable

--- Blog/Blog/Models/BlogPost.cs
+++ Blog/Blog/Models/BlogPost.cs
@@ -6,7 +6,7 @@
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
         public virtual ICollection<Comment>? Comments { get; set; }
         public string? AuthorId { get; set; }
         public BlogUser? Author { get; set; }
